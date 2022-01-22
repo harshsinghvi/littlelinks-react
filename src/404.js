98c1a11fcf0fbc4143a52feac5a76cfd77dd08c7
Harsh <harshsinghvi98@gmail.com>
2022-01-23 00:00:25 +0530

    update assets

---
+++ src/404.js
@@ -0,0 +1,67 @@
+import "./404.css";
+
+export const notFound = () => {
+  return (
+    <div className="notFound">
+      <div class="rail">
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+
+        <div class="stamp four">4</div>
+
+        <div class="stamp zero">0</div>
+      </div>
+
+      <div class="world">
+        <div class="forward">
+          <div class="box">
+            <div class="wall"></div>
+
+            <div class="wall"></div>
+
+            <div class="wall"></div>
+
+            <div class="wall"></div>
+
+            <div class="wall"></div>
+
+            <div class="wall"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
